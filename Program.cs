06e7e28b612ccf8c6ba5c477b84a1dfd8772792a
revenesbernardo1-create <revenesbernardo1@gmail.com>
2026-04-15 22:32:12 +0800

    Update Program.cs
    Sa part po na to, sinetup kopo muna yung mga kailangan para gumana yung program tulad ng Item class para ma-organize po yung details ng bawat product like name, price, at stock, taposs po gumawa ako ng function na ShowItem() pqra ma-display nang maayos yung items at ComputeTotal() para po sa pag compute ng total, tapos sa Main gumawa po ako ng array na itemsList para sa mga products at nagdeclare din ako ng arrays na selectedIds, selectedQty, at subTotals po para sa cart kahit medyo nalilito pa ako sa paggamit nila, tapos yung totalItems para matrack kung ilan napo yung napili at yung userChoice ginagamit ko para malaman kung magdadagdag pa ng item o hindi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using System;
-
-namespace quizkopo
+namespace quiz
 {
     class Item
     {
@@ -11,7 +10,12 @@ namespace quizkopo
 
         public void ShowItem()
         {
-            Console.WriteLine($"{itemCode} {itemName} {itemPrice} {stockLeft}");
+            Console.WriteLine($"{itemCode, -5} {itemName, -15} {itemPrice, -10} {stockLeft, -10}");
+        }
+
+        public double ComputeTotal(int amount)
+        {
+            return itemPrice * amount;
         }
     }
 
@@ -19,16 +23,17 @@ namespace quizkopo
     {
         static void Main(string[] args)
         {
-            Item[] itemsList = new Item[]
-            {
-                new Item { itemCode=1, itemName="Mouse", itemPrice=350, stockLeft=50 },
-                new Item { itemCode=2, itemName="Keyboard", itemPrice=800, stockLeft=30 }
+            Item[] itemsList = new Item[] {
+            new Item { itemCode=1, itemName="Laptop", itemPrice=35000, stockLeft=10 },
+            new Item { itemCode=2, itemName="Smartphone", itemPrice=18000, stockLeft=15 },
+            new Item { itemCode=3, itemName="Tablet", itemPrice=12000, stockLeft=12 },
+            new Item { itemCode=4, itemName="Smartwatch", itemPrice=5000, stockLeft=20 },
+            new Item { itemCode=5, itemName="Bluetooth Speaker", itemPrice=2500, stockLeft=25 }
             };
 
-            for (int i = 0; i < itemsList.Length; i++)
-            {
-                itemsList[i].ShowItem();
-            }
-        }
-    }
-}
+            int[] selectedIds = new int[10];
+            int[] selectedQty = new int[10];
+            double[] subTotals = new double[10];
+            int totalItems = 0;
+
+            string userChoice = "Y";
